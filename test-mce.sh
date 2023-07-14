a99b8de4370a79adfc43f36dfd9955d2dcf6052e
Borball Zhai <borball.zh@gmail.com>
2023-07-14 15:17:28 -0400

    update mce sample config

--- test-mce.sh
+++ test-mce.sh
@@ -2,7 +2,7 @@
 
 ssh 192.168.58.14 kcli stop vm mce
 ssh 192.168.58.14 kcli delete vm mce -y
-ssh 192.168.58.14 'kcli create vm -P uuid=11111111-1111-1111-1234-000000000001 -P start=False -P memory=65536 -P numcpus=24 -P disks=[120,120,120] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:86\"}"] mce'
+ssh 192.168.58.14 'kcli create vm -P uuid=11111111-1111-1111-1234-000000000001 -P start=False -P memory=40960 -P numcpus=24 -P disks=[120,120,120] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:86\"}"] mce'
 ssh 192.168.58.14 kcli list vm
 
 systemctl restart sushy-tools.service
