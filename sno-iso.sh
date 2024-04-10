bb46c954af7f7726609c48efc5d4631540bed6d3
Borball Zhai <borball.zh@gmail.com>
2024-04-10 11:08:03 -0400

    fix ztp-hub

--- sno-iso.sh
+++ sno-iso.sh
@@ -232,11 +232,12 @@ setup_ztp_hub(){
   if [ "true" = "$(yq '.day1.ztp_hub' $config_file)" ]; then
     info "ZTP Hub(LVM, RHACM, GitOps, TALM):" "enabled"
     cp $operators/lvm/*.yaml $cluster_workspace/openshift/
-    cp $operators/gitops/*.yaml $cluster_workspace/openshift/
     cp $operators/rhacm/*.yaml $cluster_workspace/openshift/
     jinja2 $operators/lvm/StorageLVMSubscription.yaml.j2 > $cluster_workspace/openshift/StorageLVMSubscription.yaml
     jinja2 $operators/rhacm/AdvancedClusterManagementSubscription.yaml.j2 > $cluster_workspace/openshift/AdvancedClusterManagementSubscription.yaml
     jinja2 $operators/talm/TopologyAwareLifeCycleManagerSubscription.yaml.j2 > $cluster_workspace/openshift/TopologyAwareLifeCycleManagerSubscription.yaml
+    jinja2 $operators/gitops/GitopsSubscription.yaml.j2 > $cluster_workspace/openshift/GitopsSubscription.yaml
+
     echo
   fi
 }
