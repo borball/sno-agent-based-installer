18b8162765bc22c73045999adc699b1248da0e6d
Borball Zhai <borball.zh@gmail.com>
2024-06-11 11:58:53 -0400

    4.16 prega catalog source support

--- sno-iso.sh
+++ sno-iso.sh
@@ -279,7 +279,7 @@ apply_extra_manifests(){
 operator_catalog_sources(){
   if [[ $(yq '.container_registry' $config_file) != "null" ]]; then
     if [ "true" = "$(yq '.container_registry.prega' $config_file)" ]; then
-      info "prega:" "enabled"
+      info "PreGA" "enabled"
       cp $templates/day1/prega/*.yaml $cluster_workspace/openshift/
     fi
 
