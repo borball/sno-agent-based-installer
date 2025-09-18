dfb87a3ef2404acb31c8d6adb1b67d41fc531866
Borball Zhai <borball.zh@gmail.com>
2025-09-18 10:51:45 -0400

    fix local-storage provision

--- templates/day1/local-storage/pre-setup.sh
+++ templates/day1/local-storage/pre-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# use to set env variables for 60-create-lvs-mc.yaml.j2
+
 config_file=$1
 
 basedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -15,5 +17,6 @@ if [[ ! -f $config_file ]]; then
 fi
 
 export CREATE_LVS_FOR_SNO=$(cat $basedir/create_lvs_for_lso.sh |base64 -w0)
-export DISK=$(yq '.operators.local-storage.provision.disk_by_path' $config_file)
-export LVS=$(yq ".operators.local-storage.provision.${partitions_key}|to_entries|map(.value + \"x\" + .key)|join(\" \")" $config_file)
+export DISK=$(yq '.day1.operators.local-storage.provision.data.disk_by_path' $config_file)
+export LVS=$(yq ".day1.operators.local-storage.provision.data.partitions|to_entries|map(.value + \"x\" + .key)|join(\" \")" $config_file)
+
